04cf0936f15e355024b24421da7c37bb3356163b
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-08-16 01:00:21 +0300

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,27 @@
-import React from "react";
+// import React from "react";
 
-function About() {
+// function About() {
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       <p>I Made This</p>
+//       <img src={image} alt="I made this" />
+//     </div>
+//   );
+// }
+// export default About
+
+// src/components/About.jsx
+
+import React from 'react';
+
+const About = () => {
   return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+    <div>
+      <h1>About Us</h1>
+      <p>We are a company dedicated to providing the best services to our customers.</p>
     </div>
   );
-}
+};
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+// import React from "react";
 
-function App() {
+// import About from "./About";
+
+// function App() {
+//   return (
+//     <div>
+//       <NavBar />
+//       <Home />
+//       <About />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/components/App.jsx
+
+import React from 'react';
+
+import About from './About'; // Adjust the import path as necessary
+
+const App = () => {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <header>
+        <h1>Welcome to Our Application</h1>
+      </header>
+      <main>
+        <About />
+      </main>
     </div>
   );
-}
+};
 
 export default App;
+
